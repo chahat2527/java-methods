7163107e38526a0c4cc443bf3aa8bd49fa7c7542
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-02 15:30:52 +1000

    first class

---
+++ static.java
@@ -0,0 +1,19 @@
+//class 1
+//Helper class
+public class Helper {
+    // Static method
+    public static int sum(int a, int b) {
+        // simply returning the sum
+        return a + b;
+    }
+}
+
+// class 2
+// CAC class
+public class CAC {
+    public static void main(String[] args) {
+        // Sum of the integer
+        int n = 6, m = 3;
+        System.out.println("sum of= " + s);
+    }
+}
