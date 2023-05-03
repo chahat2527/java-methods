f8c0571a690f70ee9b4bd99b5887396dcc6aef69
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-03 16:27:46 +1000

    commit file

---
+++ equal.java
@@ -0,0 +1,11 @@
+public class CAC {
+    public static void main(String[] args){
+        //creating a short object
+        short a= new short(20);
+        //creating a short object
+        short b= new short(20);
+        //equal methods in short class
+        boolean output =a.equal(b);
+        System.out.println(" Does " + a + " equal " + b + " : " + " output ");
+    }
+}
