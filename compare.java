8b55bc62c3bf833192ac1a4d2b5297d89fd64747
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-03 16:10:40 +1000

    class file

---
+++ compare.java
@@ -0,0 +1,19 @@
+public class CAC {
+    // derived code
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        // 10 is less the 20
+        // output value will be less than 1
+        System.out.println("integer.compare(a,b)");
+        int x = 50;
+        int y = 50;
+        // 50 is equal to 50
+        // output value will be 0
+        System.out.println("integer.compare(x,y)");
+        int w = 30;
+        int v = 20;
+        // 30 is greater than 20;
+        // output value will be a greater than 0
+    }
+}
---
+++ string.java
@@ -0,0 +1,9 @@
+public class CAC{
+    public static void main(String[] args)
+    {
+        //creating a string builderobject
+        string builder str+ new string builder("java");
+        //print add display the string 
+        System.out.println("String contains= str.to string");
+    }
+}
